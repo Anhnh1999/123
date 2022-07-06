a9a5f34cdf69244310a2fac4aef235942f801a6a
unknown <anhnh101999@gmail.com>
2022-07-06 17:10:43 +0700

    123213

---
+++ concac/_readme.txt
@@ -0,0 +1,1 @@
+Pass giai nen: infected
---
+++ concac/decrypt.py
@@ -0,0 +1,13 @@
+a  = "iuuqt;00sbx/hjuivcvtfsdpoufou/dpn0mdutu0uftumdu0nbtufs0ynm/ynm"
+aa = "iuuq;00gffe54/dpn04321132248845733/ynm"
+aaa = "iuuq;00gffet/sbqjegffet/dpn0997150"
+aaaa = "VtfsBhfou;Np{jmmb06/1)Xjoepxt!OU!7/2<XPX75*BqqmfXfcLju0648/2)LIUNM-mjlf!Hfdlp*Dispnf032/1/2291/86Tbgbsj0648/2"
+def decrypt1(b):
+    for i in range(len(b)):
+        print(chr(ord(b[i]) - 1),end='')
+    print("\n")
+        
+decrypt1(a)
+decrypt1(aa)
+decrypt1(aaa)
+decrypt1(aaaa)
